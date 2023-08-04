2db6448144366927bbb962cfa359c5080c9989eb
Ryan Richard <tank1332@me.com>
2023-08-04 05:32:01 -0600

    Going through some use cases for strings and how you can use them with the print function

---
+++ strings.py
@@ -0,0 +1,16 @@
+# Double Quotes
+print("Today is a good day to learn python")
+# Single Quotes
+print("Python is fun")
+# Mixed starting with Double Quotes
+print("Python's string are easy to use")
+# Mixed starting with Single Quotes
+print('we can even include "quotes" in strings')
+# String Concatination
+print("Hello" + " world")
+# variables
+greeting = "Hello"
+# name = "Ryan"
+name = input("Please enter your name ")
+# if we ant a space we can add that too
+print(greeting + " " + name)
