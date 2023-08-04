dac94c48596bd7ea0f4691ad86ba0c7bb08cf829
Ryan Richard <tank1332@me.com>
2023-08-04 05:53:55 -0600

    working through excercises in course

---
+++ exercises/tabs.py
@@ -0,0 +1,14 @@
+# Printing tabs
+# Write a program that will produce the following output.  All the text should appear in your program's output.
+
+# Example Output
+# Number 1      The Larch
+# Number 2      The Horse Chestnut
+
+
+# Use a tab character to line up the name of the trees.  Note that there's no need to use variables, unless you want to.
+n1 = "Number 1\tThe Larch"
+n2 = "Number2\tThe Horse Chestnut"
+
+print(n1)
+print(n2)
