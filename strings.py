d489f449d04391a607beaa274fd73f3a4aeb4cb1
Ryan Richard <tank1332@me.com>
2023-08-04 06:09:37 -0600

    Added some learning around errors with concatenated strings.

--- strings.py
+++ strings.py
@@ -8,10 +8,31 @@ print("Python's string are easy to use")
 print('we can even include "quotes" in strings')
 # String Concatination
 print("Hello" + " world")
-# variables
+
+# Variables
 greeting = "Hello"
-# name = "Ryan"
-name = input("Please enter your name ")
+name = "Ryan"
+
+# Below is how you get user input
+# name = input("Please enter your name ")
+
 # if we want a space we can add that too
 print(greeting + " " + name)
 print(greeting, name)
+
+age = 24
+print(age)
+
+# How to show what class type an integer is.
+print(type(greeting))
+print(type(age))
+
+# Reassigning age and testing what the type is.
+age_in_words = "2 years"
+
+# Will produce a TypeError
+# print(name + " is " + age + " old")
+
+# Below code will not error out.
+print(name + " is " + age_in_words + " old")
+print(type(age))
