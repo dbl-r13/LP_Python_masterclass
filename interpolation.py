b0cd1c5c310c1197a97149d04743e0cc74cf09c0
Ryan Richard <tank1332@me.com>
2023-08-08 04:56:33 -0600

    Python 2 examples of Print Interpolation

---
+++ interpolation.py
@@ -0,0 +1,14 @@
+# Python 2 examples
+
+age = 24
+print("may age is %d years" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))
+print("PI is approximately %f" % (22 / 7))
+print("PI is approximately %60.50f" % (22 / 7))
+# Different types you can use for interpolation
+# %x hexidecimal
+# %o octal
+# %e scientific notification
